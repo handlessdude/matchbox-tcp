0a90054a397e452bec97df81d8956c9fd41ee171
Elis <handlessdude@gmail.com>
2022-05-11 16:28:41 +0300

    implement game loop2

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@
 require 'socket'
 
 clients = {}
-actions = ['1', '2', '3']
+actions = %w[1 2 3]
 game_state = {
   matches: 33,
   lastTurn: {
@@ -23,13 +23,14 @@ reset_game = lambda {
 two_players_appeared = -> { clients.length>1 }
 action_reducer = lambda { | client_id, action |
   return false unless actions.include? action
+
   new_matches = game_state[:matches]
   if client_id == game_state[:lastTurn][:client_id] && game_state[:matches]>1
     new_matches += game_state[:lastTurn][:action].to_i
   end
   new_matches -= action.to_i
   set_game_state.call(new_matches, client_id, action)
-  return true
+  true
 }
 
 
@@ -44,6 +45,9 @@ begin
         client.puts 'Too many clients on the server. Try join later',
                     'Server connection terminated'
         client.close
+        # return 0
+        # break
+        # else
       end
 
       id = :"player_#{clients.length + 1}"
@@ -51,11 +55,13 @@ begin
       puts "=> Client #{id} arrived."
 
       client.puts 'Server connection established!',
-                  "Your id: #{id}",
-                  "Clients on the server right now: #{clients.length}#{two_players_appeared.call ? '. Two players appeared. GAME START!' : ''}"
+                  "Your id: #{id}"
 
-      while (action = client.gets)
-        # break if line =~ /quit/
+      clients.each do |client_id, client|
+        client.puts "Clients on the server right now: #{clients.length}#{two_players_appeared.call ? '. Two players appeared. GAME START!' : ''}"
+      end
+
+      while (action = client.gets.strip)
         unless two_players_appeared.call
           client.puts 'Second player did not arrived yet. Be patient!'
           next
@@ -64,25 +70,23 @@ begin
         if action_reducer.call(id, action)
           game_is_over = (game_state[:matches] <= 0)
           message = if game_is_over
-                      "Game is over!\n#{game_state[:lastTurn][:client_id]} has taken all the matches!\nThe winner is #{clients.detect { |client_id| client_id != game_state[:lastTurn][:client_id] } }!"
+                      "Game is over!\n#{game_state[:lastTurn][:client_id]} has taken all the matches!\nThe winner is #{clients.keys.detect { |client_id | !(client_id == game_state[:lastTurn][:client_id]) } }!"
                     else
-                      "client #{id} #{am_i_prev_player ? 'changes their mind and ' : ''} takes #{action} matches.\nMatches on the table: #{game_state[:matches]}"
+                      "#{id} #{am_i_prev_player ? 'changes their mind and ' : ''} takes #{action} matches.\nMatches on the table: #{game_state[:matches]}"
                     end
-          client.puts message
-          if game_is_over
-            reset_game.call
-            clients.each { |client_id, client|
-              client.puts 'Server connection terminated'
-              puts "<= Client #{id} has left."
-              client.close
-              clients.delete(id)
-            }
+          clients.each do |client_id, client|
+            client.puts message
           end
+          break if game_is_over
         else
           client.puts "Unknown action: #{action}. Try something else!"
         end
-
       end
+
+      client.puts 'Server connection terminated'
+      puts "<= Client #{id} has left."
+      clients.delete(id)
+      client.close
 
     end
   end
